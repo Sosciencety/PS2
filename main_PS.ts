c509ac45fd2f0fb49209982d21fd72383c21577a
Sosciencety <gazagul@gmail.com>
2022-01-29 09:41:51 +0700

    Update main_PS.ts

--- main_PS.ts
+++ main_PS.ts
@@ -2,10 +2,10 @@
 //% color="#31C7D5" weight=10 icon="\uf1d1"
  namespace ps2controller {
 
-    let chipSelect = DigitalPin.P6
+    let chipSelect = DigitalPin.P0
     pins.digitalWritePin(chipSelect, 1)
 
-    pins.spiPins(DigitalPin.P9, DigitalPin.P8, DigitalPin.P7)
+    pins.spiPins(DigitalPin.P8, DigitalPin.P5, DigitalPin.P1)
     pins.spiFormat(8, 3)
     pins.spiFrequency(250000)
 
